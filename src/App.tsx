2224b87cc3b475691e37282d65730787773a5a4c
breathinssum <shtnals1619@hanmail.net>
2022-02-05 12:25:45 +0900

    코로나 api요청

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Sidebar from './Components/Sidebar';
 import Summary from './Components/Summary';
@@ -6,6 +7,8 @@ import "./index.css"
 import Korea from './Pages/Korea';
 
 function App() {
+
+
   return (
     <div className="App">
       <Sidebar/>
