0cf02e91eb10df70b84745e9aac94779a8f37502
breathinssum <shtnals1619@hanmail.net>
2022-02-19 12:41:37 +0900

    해외현황추가

--- src/@types/summary.d.ts
+++ src/@types/summary.d.ts
@@ -1,17 +1,41 @@
 interface SummaryType {
-    countryNm: string; // 도시명
-    deathCnt: number; // 사망자 수
-    incDec: number; // 전일대비 확진
-    incDecF: number; // 전일대비 확진 - 해외
-    incDecK: number; // 전일대비 확진 - 국내
-    isolCnt: number; // 치료중인 확진자 수
-    qurRate: number; // 코로나 발생률
-    recCnt: number; // 완치자 수
-    totalCnt: number; // 전체 확진자 수
-    deathPcnt: number; // 사망률
-    recPcnt: number; // 완치율
+  countryNm: string; // 도시명
+  deathCnt: number; // 사망자 수
+  incDec: number; // 전일대비 확진
+  incDecF: number; // 전일대비 확진 - 해외
+  incDecK: number; // 전일대비 확진 - 국내
+  isolCnt: number; // 치료중인 확진자 수
+  qurRate: number; // 코로나 발생률
+  recCnt: number; // 완치자 수
+  totalCnt: number; // 전체 확진자 수
+  deathPcnt: number; // 사망률
+  recPcnt: number; // 완치율
 
-    cities : Omit<SummaryType, 'cities'>[]
+  cities: Omit<SummaryType, 'cities'>[]
+}
+
+
+interface WorldSummaryType {
+  Global: {
+    Date: string
+    NewConfirmed:number
+    NewDeaths:number
+    NewRecovered:number
+    TotalConfirmed:number
+    TotalDeaths:number
+    TotalRecovered:number
   }
-  
-  
+  Countries: {
+    Country: string
+    CountryCode: string
+    Date: string
+    ID: string
+    NewConfirmed: number
+    NewDeaths: number
+    NewRecovered: number
+    Slug: string
+    TotalConfirmed: number
+    TotalDeaths: number
+    TotalRecovered: number
+  }[]
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,20 @@
-import axios from 'axios';
-import React, { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Sidebar from './Components/Sidebar';
-import Summary from './Components/Summary';
 import "./index.css"
-import Korea from './Pages/Korea';
+import Korea from './pages/Korea';
+import World from './pages/World';
 
 function App() {
-
 
   return (
     <div className="App">
       <Sidebar/>
       <section>
         <Routes>
-        <Route path = "/" element={<Korea/>}/>
-        <Route path = "/world" element={<div>world</div>}/>
+          <Route path="/" element={<Korea/>}/>  
+          <Route path="/world" element={<World/>}/>  
         </Routes>
-      </section>
+      </section>    
     </div>
   );
 }
