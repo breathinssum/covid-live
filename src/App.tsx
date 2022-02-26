d5115a590e2d35d6ced3b56171817e464707a0bf
breathinssum <shtnals1619@hanmail.net>
2022-02-26 11:08:23 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import { Route, Routes } from 'react-router-dom';
 import Sidebar from './Components/Sidebar';
 import './index.css';
 // import Korea from './pages/Korea';
-import Vaccine from './pages/Vaccine';
-import World from './pages/World';
+// import Vaccine from './pages/Vaccine';
+// import World from './pages/World';
 
 function App() {
   return (
@@ -12,8 +12,8 @@ function App() {
       <section>
         <Routes>
           {/* <Route path="/" element={<Korea />} /> */}
-          <Route path="/world" element={<World />} />
-          <Route path="/vaccine" element={<Vaccine />} />
+          {/* <Route path="/world" element={<World />} />
+          <Route path="/vaccine" element={<Vaccine />} /> */}
         </Routes>
       </section>
     </div>
