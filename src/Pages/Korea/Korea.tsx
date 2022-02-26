3c3cc5ea1aa61d264c5c0c3591564c91d7760a2f
breathinssum <shtnals1619@hanmail.net>
2022-02-26 11:06:36 +0900

    test

--- src/Pages/Korea/Korea.tsx
+++ src/Pages/Korea/Korea.tsx
@@ -1,34 +1,30 @@
-import { useState, useCallback, useEffect } from "react";
-import Summary from "../../Components/Summary";
+import { useCallback, useEffect, useState } from "react";
+import Summary from "../../Components/Summary"
 import SummaryList from "../../Components/SummaryList";
-import TodaySummary from "../../Components/TodaySummary";
+import TodaySummary from "../../Components/TodaySummary"
 import { httpGetSummary } from "../../httpAPI";
 
 
-
-const Korea = ( ) => {
+const Korea = () => {
     const [summary, setSummary] = useState<SummaryType>();
 
-  const getSummary = useCallback(async () => {
-    const summary = await httpGetSummary();
-
-    setSummary(summary);
-  }, []);
-
-  useEffect(() => {
-    getSummary();
-  }, []);
+    const getSummary = useCallback(async () => {
+      const summary = await httpGetSummary();
+  
+      setSummary(summary);
+    }, []);
+  
+    useEffect(() => {
+      getSummary();
+    }, []);
 
     return (
         <>
-           
-           <Summary summary={summary}/>
-           <TodaySummary summary={summary}/>
-           <SummaryList summary={summary}/>
-
+            <Summary summary={summary}/>
+            <TodaySummary summary={summary}/>
+            <SummaryList summary={summary}/>
         </>
     )
-
 }
 
 export default Korea
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
@@ -12,4 +11,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
